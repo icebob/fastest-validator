67ad78cbd2b0e7745605f8f0c18c617220653599
Icebob <mereg.norbert@gmail.com>
2017-07-03 14:07:31 +0200

    cover customValidator with tests

---
+++ test/rules/custom.spec.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const Validator = require("../../lib/validator");
+const fn = require("../../lib/rules/custom");
+
+const v = new Validator();
+const check = fn.bind(v);
+
+describe("Test checkCustom", () => {
+
+	it("should call custom checker", () => {
+		const checker = jest.fn(() => true);
+		const s = { type: "custom", a: 5, check: checker };
+
+		expect(check(10, s)).toEqual(true);
+		expect(checker).toHaveBeenCalledTimes(1);
+		expect(checker).toHaveBeenCalledWith(10, s);
+	});
+
+	it("should handle returned errors", () => {
+		const checker = jest.fn(function() { 
+			return this.makeError("myError", 3, 4);
+		});
+		const s = { type: "custom", a: 5, check: checker };
+
+		expect(check(10, s)).toEqual({
+			type: "myError",
+			actual: 4, 
+			expected: 3, 
+		});
+		expect(checker).toHaveBeenCalledTimes(1);
+		expect(checker).toHaveBeenCalledWith(10, s);
+	});
+
+});
