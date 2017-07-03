27e8288c0dc25621428f2d04b11cb17b10e3d055
Icebob <mereg.norbert@gmail.com>
2017-07-03 10:40:06 +0200

    add dev example to multi

---
+++ examples/multi.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+let Validator = require("../index");
+
+let v = new Validator();
+
+const schema = {
+    multi: [
+		{ type: "string", min: 3, max: 255 },
+		{ type: "boolean" }
+	]
+};
+
+console.log(v.validate({ multi: "John" }, schema));
+// Returns: true
+
+console.log(v.validate({ multi: true }, schema));
+// Returns: true
+
+console.log(v.validate({ multi: false }, schema));
+// Returns: true
+
+console.log(v.validate({ multi: "Al" }, schema));
+/* Returns an array with errors:
+
+*/
+
+console.log(v.validate({ multi: 100 }, schema));
+/* Returns an array with errors:
+
+*/
