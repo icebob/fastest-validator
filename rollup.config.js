9fa67ccb3fd1826b47a1f3f77cf84a0c8aa3d9fb
☁ Rheza Satria <rheza.satria.ta@gmail.com>
2017-09-08 11:09:46 +0700

    chore: add browser prod minification and source map for dev mode
    1. Add closure compilation
    2. Add minification
    3. Update yarn.lock
    4. Add source map

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,60 @@
 import commonjs from "rollup-plugin-commonjs";
 import buble from "rollup-plugin-buble";
+import closure from "rollup-plugin-closure-compiler-js";
+import uglify from "rollup-plugin-uglify-es";
 import pkg from "./package.json";
 
+const BUNDLE_NAME = "FastestValidator";
+
+// transpile ES2015+ to ES5
+const bublePlugin = buble({
+	exclude: ["node_modules/**", "examples/**", "dist/**", "test/**", "benchmark/**"]
+});
+
 const bundles = [
-	// browser-friendly UMD build
+	// browser-friendly UMD build, suitable for dev
+	{
+		input: "index.js",
+		output: {
+			file: "dist/browser/index.js",
+			format: "umd",
+			name: BUNDLE_NAME
+		},
+		plugins: [
+			commonjs(),
+
+			bublePlugin
+		],
+
+		sourceMap: true
+	},
+
+	// Browser Minification version, suitable for production
 	{
 		input: "index.js",
 		output: {
 			file: pkg.browser,
 			format: "umd",
-			name: "FastestValidator"
+			name: BUNDLE_NAME
 		},
 		plugins: [
 			commonjs(),
 
-			// transpile ES2015+ to ES5
-			buble({
-				exclude: ["node_modules/**", "examples/**", "dist/**", "test/**", "benchmark/**"]
-			})
+			closure({
+				compilationLevel: "SIMPLE",
+				languageIn: "ECMASCRIPT6_STRICT",
+				languageOut: "ECMASCRIPT5_STRICT",
+				env: "BROWSER",
+				warningLevel: "QUIET",
+				assumeFunctionWrapper: true,
+				applyInputSourceMaps: false,
+				useTypesForOptimization: false,
+				processCommonJsModules: false,
+			}),
+
+			bublePlugin,
+
+			uglify()
 		]
 	}
 ];
