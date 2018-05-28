563b3be6d5a16febcff51c6ce49674316eef98bf
Maurice Doepke <fr1tzw4lt3r@googlemail.com>
2018-05-29 00:35:18 +0200

    fixes #21

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -12,7 +12,7 @@ describe("Test constructor", () => {
 		expect(v.validate).toBeInstanceOf(Function);
 		expect(v.add).toBeInstanceOf(Function);
 
-		expect(Object.keys(v.rules).length).toBe(12);
+		expect(Object.keys(v.rules).length).toBe(13);
 	});
 
 	it("should create instance with custom messages", () => {
