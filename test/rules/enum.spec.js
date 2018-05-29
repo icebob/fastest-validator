123b5e4a6e623502377869f76a503b49f8d679a2
Maurice Doepke <fr1tzw4lt3r@googlemail.com>
2018-05-29 10:33:35 +0200

    finally fixes #21

---
+++ test/rules/enum.spec.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const Validator = require("../../lib/validator");
+const fn = require("../../lib/rules/enum");
+
+const v = new Validator();
+const check = fn.bind(v);
+
+describe("Test checkEnum", () => {
+
+	it("check enum", () => {
+		const e1 = { type: "enum", values: ["male", "female"] };
+		
+		expect(check("", e1)).toEqual({ type: "enumValue", expected: ["male", "female"] });
+		expect(check("human", e1)).toEqual({ type: "enumValue", expected: ["male", "female"] });
+		expect(check("male", e1)).toEqual(true);
+		expect(check("female", e1)).toEqual(true);
+
+		const e2 = { type: "enum", values: [null, 1, 2, "done", false] };
+		expect(check("male", e2)).toEqual({ type: "enumValue", expected: [null, 1, 2, "done", false] });
+		expect(check(null, e2)).toEqual(true);
+		expect(check(2, e2)).toEqual(true);
+		expect(check("done", e2)).toEqual(true);
+		expect(check(false, e2)).toEqual(true);
+	});
+
+	}
+);
