c4a8d1076b4f66d52b9ee593e8f0dab130e39e14
Icebob <mereg.norbert@gmail.com>
2019-10-27 22:20:16 +0100

    custom rule

--- examples/next.js
+++ examples/next.js
@@ -1,16 +1,20 @@
 let Validator = require("../index");
 
-let v = new Validator();
+let v = new Validator({
+	messages: {
+		stringMin: "A(z) '{field}' mező túl rövid. Minimum: {expected}, Jelenleg: {actual}"
+	}
+});
 
 const schema = {
 	//id: { type: "number", positive: true, integer: true, convert: true },
 	//name: { type: "string", min: 3, max: 255 },
 	//password: { type: "forbidden" },
 	//roles: { type: "array", items: "string", min: 1 },
-	friends: { type: "array", items: { type: "object", properties: {
+	/*friends: { type: "array", items: { type: "object", properties: {
 		name: "string",
 		username: "string"
-	}}},
+	}}},*/
 	/*address: { type: "object", properties: {
 		country: "string",
 		city: "string",
@@ -19,10 +23,23 @@ const schema = {
 	//email: { type: "email", mode: "precise" },
 	//status: "boolean" // short-hand def
 	//status: { type: "boolean", convert: true }
-	status: [
+	/*status: [
 		{ type: "boolean" },
 		{ type: "number" }
-	]
+	],*/
+
+	weight: {
+		type: "custom",
+		minWeight: 10,
+		check(value, schema, field) {
+			return (value < schema.minWeight)
+				? [{ type: "weightMin", field, expected: schema.minWeight, actual: value }]
+				: true;
+		},
+		messages: {
+			weightMin: "The '${field}' must be greater than {expected}! Actual: {actual}"
+		}
+	}
 };
 
 const check = v.compile(schema);
@@ -40,9 +57,11 @@ console.log("John", check({
 	roles: ["admin", "user", 3, 2, true],
 	friends: [
 		{ name: "John", username: "johnny" },
-		{ name: "Jane", _username: "jane" }
+		{ name: "Jane", username: "jane" }
 	],
 	status: 1,
+
+	weight: 10,
 }));
 
 //console.log("Al", check({ id: "1", name: "Al", status: false, email: "a@b.cc" }));
