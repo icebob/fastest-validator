1b2aabc85f6fb701d6feab12e9e863855a1e0916
Icebob <mereg.norbert@gmail.com>
2019-10-27 16:37:11 +0100

    rewrite

--- examples/next.js
+++ examples/next.js
@@ -6,6 +6,11 @@ const schema = {
 	//id: { type: "number", positive: true, integer: true, convert: true },
 	name: { type: "string", min: 3, max: 255 },
 	password: { type: "forbidden" },
+	roles: { type: "array", items: "string", length: 2, enum: ["admin", "user"] },
+	address: { type: "object", props: {
+		country: "string",
+		city: "string"
+	} },
 	//email: { type: "email", mode: "precise" },
 	//status: "boolean" // short-hand def
 	//status: { type: "boolean", convert: true }
@@ -14,6 +19,17 @@ const schema = {
 const check = v.compile(schema);
 
 console.log("============");
-console.log("John", check({ id: 5, name: "John", status: true, email: "john.doe@moleculer.services" }));
-console.log("Al", check({ id: "1", name: "Al", status: false, email: "a@b.cc" }));
+console.log("John", check({
+	id: 5,
+	name: "John",
+	email: "john.doe@moleculer.services",
+	address: {
+		country: "Hungary",
+		zip: 1112
+	},
+	roles: ["admin", "user"],
+	status: true,
+}));
+
+//console.log("Al", check({ id: "1", name: "Al", status: false, email: "a@b.cc" }));
 
