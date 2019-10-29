50838b42b62013aba1d782e65ef496f9574bec8d
Icebob <mereg.norbert@gmail.com>
2019-10-29 23:25:46 +0100

    custom test

--- test/rules/custom.spec.js
+++ test/rules/custom.spec.js
@@ -1,35 +1,44 @@
 "use strict";
 
 const Validator = require("../../lib/validator");
-const fn = require("../../lib/rules/custom");
-
 const v = new Validator();
-const check = fn.bind(v);
 
-describe("Test checkCustom", () => {
+describe("Test rule: custom", () => {
 
 	it("should call custom checker", () => {
 		const checker = jest.fn(() => true);
-		const s = { type: "custom", a: 5, check: checker };
+		const schema = { $$root: true, type: "custom", a: 5, check: checker };
+		const check = v.compile(schema);
 
-		expect(check(10, s)).toEqual(true);
+		expect(check(10)).toEqual(true);
 		expect(checker).toHaveBeenCalledTimes(1);
-		expect(checker).toHaveBeenCalledWith(10, s);
+		expect(checker).toHaveBeenCalledWith(10, schema, "null", undefined, 10);
+	});
+
+	it("should call custom checker", () => {
+		const checker = jest.fn(() => true);
+		const schema = { weight: { type: "custom", a: 5, check: checker } };
+		const check = v.compile(schema);
+
+		expect(check({ weight: 10 })).toEqual(true);
+		expect(checker).toHaveBeenCalledTimes(1);
+		expect(checker).toHaveBeenCalledWith(10, schema.weight, "weight", { weight: 10 }, { weight: 10 });
 	});
 
 	it("should handle returned errors", () => {
-		const checker = jest.fn(function() { 
-			return this.makeError("myError", 3, 4);
+		const checker = jest.fn(function(value, schema, field) {
+			return [{ type: "myError", field, expected: 3, actual: 4 }];
 		});
-		const s = { type: "custom", a: 5, check: checker };
+		const schema = { weight: { type: "custom", a: 5, check: checker } };
+		const check = v.compile(schema);
 
-		expect(check(10, s)).toEqual({
+		expect(check({ weight: 10 })).toEqual([{
 			type: "myError",
-			actual: 4, 
-			expected: 3, 
-		});
+			actual: 4,
+			expected: 3,
+		}]);
 		expect(checker).toHaveBeenCalledTimes(1);
-		expect(checker).toHaveBeenCalledWith(10, s);
+		expect(checker).toHaveBeenCalledWith(10, schema.weight, "weight", { weight: 10 }, { weight: 10 });
 	});
 
 });
