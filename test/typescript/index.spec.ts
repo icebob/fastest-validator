9a9df8f99928e02edd38c069913ce8abf2da211c
Andrew Kazakou <andrew.kazakou@pluto.tv>
2019-11-11 21:03:36 +0300

    Move TypeScript unit tests up to test/typescript directory

--- test/typescript/index.spec.ts
+++ test/typescript/index.spec.ts
@@ -1,7 +1,7 @@
-/// <reference path="../../../index.d.ts" /> // here we make a reference to exists module definition
+/// <reference path="../../index.d.ts" /> // here we make a reference to exists module definition
 import ValidatorType from 'fastest-validator'; // here we importing type definition of default export
 
-const Validator: typeof ValidatorType = require('../../../index'); // here we importing real Validator Constructor
+const Validator: typeof ValidatorType = require('../../index'); // here we importing real Validator Constructor
 
 describe('Typescript Definitions', () => {
     it('should compile validator', async () => {
