e20fa7085e78669b99d136b2a0a36d503fbcf932
ERFANIUM <officialerfanshield@gmail.com>
2020-06-14 18:08:22 +0430

    improve custom validation

--- test/rules/multi.spec.js
+++ test/rules/multi.spec.js
@@ -1,9 +1,30 @@
 "use strict";
 
 const Validator = require("../../lib/validator");
-const v = new Validator();
+const v = new Validator({
+	useNewCustomCheckerFunction: true
+});
 
 describe("Test rule: multi", () => {
+	it("should call item's custom checker function", () => {
+		const fn = jest.fn((v) => v);
+
+		const check = v.compile({
+			$$root: true,
+			type: "multi", 
+			rules: [{
+				type: "string",
+				custom: fn
+			}, {
+				type: "number",
+				custom: fn
+			}]
+		});
+
+		check("s");
+		expect(fn).toBeCalledTimes(1);
+
+	});
 
 	it("should value equals to other field", () => {
 		// TODO: move from validator.spec.js
