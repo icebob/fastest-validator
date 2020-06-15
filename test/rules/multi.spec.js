bbf5ccc24607d021da6f6c98e22fce21465ecee7
Icebob <mereg.norbert@gmail.com>
2020-06-15 20:58:37 +0200

    add failed test for custom multi #155

--- test/rules/multi.spec.js
+++ test/rules/multi.spec.js
@@ -9,9 +9,9 @@ describe("Test rule: multi", () => {
 	it("should call item's custom checker function", () => {
 		const fn = jest.fn((v) => v);
 
-		const check = v.compile({
+		const schema = {
 			$$root: true,
-			type: "multi", 
+			type: "multi",
 			rules: [{
 				type: "string",
 				custom: fn
@@ -19,11 +19,13 @@ describe("Test rule: multi", () => {
 				type: "number",
 				custom: fn
 			}]
-		});
+		};
+
+		const check = v.compile(schema);
 
 		check("s");
 		expect(fn).toBeCalledTimes(1);
-
+		expect(fn).toBeCalledWith("s", [], schema.rules[0], "$$root", null, expect.any(Object));
 	});
 
 	it("should value equals to other field", () => {
